de6e0c0249de6358f31dd2a442542be452d4a965
Axel Magnuson <axelmagn@gmail.com>
2016-11-23 14:02:14 -0700

    WIP write docopt usage parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+zDocker.
+
+Usage:
+    zdocker <env> <args>...
+
+Options:
+    -h --help   Show this screen.
+    --version   Show version.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_env: Vec<String>,
+    arg_args: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+                             .and_then(|d| d.decode())
+                             .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
